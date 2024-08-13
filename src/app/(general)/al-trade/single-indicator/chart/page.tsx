0668ecf8ae72e1a39281fdf2393e789aaa5173f3
tidvn <tiendung0325@gmail.com>
2024-08-13 11:44:00 +0700

    add tradingview lib

---
+++ src/app/(general)/al-trade/single-indicator/chart/page.tsx
@@ -0,0 +1,61 @@
+"use client";
+// import { HeatMapChart } from "@/app/components/rsi-heatmap/heatmap";
+import { ManagePortfolio } from "@/app/components/rsi-heatmap/manage-portfolio";
+import { OderInfomation } from "@/app/components/rsi-heatmap/order-info";
+import { TopOverBought } from "@/app/components/rsi-heatmap/top-over-bought";
+import { TopOverSold } from "@/app/components/rsi-heatmap/top-over-sold";
+import { TradeWithCandleStick } from "@/app/components/rsi-heatmap/trade-with-candle-stick";
+import { TradingStrategy } from "@/app/components/rsi-heatmap/trading-strategy";
+import TradingViewComp from "@/app/components/TradingView/TradingView";
+import { ArrowsOutSimple } from "@phosphor-icons/react/dist/ssr";
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
+
+export default function ChartPage() {
+  const handle = useFullScreenHandle();
+
+  return (
+    <div className="max-w-8xl">
+      <div className="grid grid-cols-3 lg:grid-cols-4 gap-x-4 gap-y-5 mt-6">
+        <div className="col-span-3 lg:col-span-1 order-2 lg:order-1 border border-[#E7E7E7] rounded-xl">
+          <TopOverSold />
+        </div>
+        <div className="col-span-3 lg:col-span-2 order-1 lg:order-2 border border-[#E7E7E7] rounded-xl relative">
+          {/* <ArrowsOutSimple
+            weight="bold"
+            width={24}
+            height={24}
+            className="absolute right-6 top-8 z-20 cursor-pointer"
+            onClick={handle.enter}
+          /> */}
+          {/* <div
+            // handle={handle}
+            className="flex items-center justify-around h-full"
+          > */}
+            <TradingViewComp />
+          {/* </div> */}
+        </div>
+        <div className="col-span-3 lg:col-span-1 order-3 border border-[#E7E7E7] rounded-xl">
+          <TopOverBought />
+        </div>
+      </div>
+      <div className="grid grid-cols-3 lg:grid-cols-4 gap-x-4 gap-y-5 mt-6">
+        <div className="col-span-3 lg:col-span-1 border border-[#E7E7E7] rounded-xl">
+          <OderInfomation />
+        </div>
+        <div className="col-span-3 lg:col-span-2 border border-[#E7E7E7] rounded-xl">
+          <TradingStrategy />
+        </div>
+        <div className="col-span-3 lg:col-span-1">
+          <div className="flex flex-col justify-between gap-4 h-full">
+            <div className="border border-[#E7E7E7] rounded-xl">
+              <TradeWithCandleStick />
+            </div>
+            <div className="grow border border-[#E7E7E7] rounded-xl">
+              <ManagePortfolio />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/components/TVChartContainer/index.tsx
@@ -0,0 +1,93 @@
+import styles from "./index.module.css";
+import { useEffect, useRef } from "react";
+import { ChartingLibraryWidgetOptions, LanguageCode, ResolutionString, widget } from "@/public/static/charting_library";
+
+export const TVChartContainer = (props: Partial<ChartingLibraryWidgetOptions>) => {
+	const chartContainerRef =
+		useRef<HTMLDivElement>() as React.MutableRefObject<HTMLInputElement>;
+
+	useEffect(() => {
+		const widgetOptions: ChartingLibraryWidgetOptions = {
+			symbol: props.symbol,
+			datafeed: new (window as any).Datafeeds.UDFCompatibleDatafeed(
+				"https://demo_feed.tradingview.com",
+				undefined,
+				{
+					maxResponseLength: 1000,
+					expectedOrder: "latestFirst",
+				}
+			),
+			interval: props.interval as ResolutionString,
+			container: chartContainerRef.current,
+			library_path: props.library_path,
+			locale: props.locale as LanguageCode,
+			disabled_features: [
+				"use_localstorage_for_settings",
+				"header_symbol_search",
+				"header_compare",
+				"left_toolbar",
+				"timeframes_toolbar"
+			],
+			enabled_features: ["study_templates"],
+			charts_storage_url: props.charts_storage_url,
+			charts_storage_api_version: props.charts_storage_api_version,
+			client_id: props.client_id,
+			user_id: props.user_id,
+			fullscreen: props.fullscreen,
+			autosize: props.autosize,
+			theme: "light", 
+			toolbar_bg: "#FFFFFF",
+			overrides: {
+				"paneProperties.background": "#FFFFFF", // Màu nền của biểu đồ
+				"paneProperties.vertGridProperties.color": "#E6E6E6", // Màu lưới dọc
+				"paneProperties.horzGridProperties.color": "#E6E6E6", // Màu lưới ngang
+				"scalesProperties.textColor": "#000", // Màu văn bản của các thang đo
+				// "mainSeriesProperties.candleStyle.upColor": "#4CAF50", // Màu nến tăng
+				// "mainSeriesProperties.candleStyle.downColor": "#E91E63", // Màu nến giảm
+				// "mainSeriesProperties.candleStyle.borderUpColor": "#4CAF50", // Màu viền nến tăng
+				// "mainSeriesProperties.candleStyle.borderDownColor": "#E91E63", // Màu viền nến giảm
+				// "mainSeriesProperties.candleStyle.wickUpColor": "#4CAF50", // Màu bấc nến tăng
+				// "mainSeriesProperties.candleStyle.wickDownColor": "#E91E63" // Màu bấc nến giảm
+			},
+			// studies_overrides: {
+			// 	"volume.volume.color.0": "rgba(255, 74, 104, 1)", // Màu cột volume giảm
+			// 	"volume.volume.color.1": "rgba(81, 234, 138, 1)", // Màu cột volume tăng
+			// 	"volume.volume.transparency": 30, // Độ trong suốt của volume
+			// 	"volume.volume.price_levels": 50 // Độ dày của mức giá
+			// }
+		};
+		
+		
+
+		const tvWidget = new widget(widgetOptions);
+
+		tvWidget.onChartReady(() => {
+			tvWidget.headerReady().then(() => {
+				const button = tvWidget.createButton();
+				button.setAttribute("title", "Click to show a notification popup");
+				button.classList.add("apply-common-tooltip");
+				button.addEventListener("click", () =>
+					tvWidget.showNoticeDialog({
+						title: "Notification",
+						body: "TradingView Charting Library API works correctly",
+						callback: () => {
+							console.log("Noticed!");
+						},
+					})
+				);
+
+				button.innerHTML = "Check API";
+			});
+		});
+
+		return () => {
+			tvWidget.remove();
+		};
+	}, [props]);
+
+	return (
+		<>
+			<div ref={chartContainerRef} className={styles.TVChartContainer} />
+		</>
+	);
+};
